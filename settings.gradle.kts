1871e67f198d2c1dae21e64dd2c9e068ca95d88e
Tyler Bertrand <tbertrand@gradle.com>
2024-01-26 14:30:50 -0600

    Connect to Develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,21 @@
+plugins {
+    id("com.gradle.enterprise") version "3.16.1"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.12.1"
+}
+
+val isCI = !System.getenv("CI").isNullOrEmpty() // adjust to your CI provider
+
+gradleEnterprise {
+    server = "https://ge.solutions-team.gradle.com" // adjust to your Develocity server
+    allowUntrustedServer = false // ensure a trusted certificate is configured
+
+    buildScan {
+        capture { isTaskInputFiles = true }
+        isUploadInBackground = !isCI
+        publishAlways()
+    }
+}
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
